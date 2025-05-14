ade4e32a9c7d63eb7d205ffdb3fbed74fe17a285
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-05-14 08:35:27 +0000

    1631 path with minimum effort binary search & breadth first search

--- src/leetcode/n1631/solution.js
+++ src/leetcode/n1631/solution.js
@@ -1,8 +1,10 @@
 /**
+ * binary search
+ * deep first search
  * @param {number[][]} heights
  * @returns {number}
  */
-function minimumEffortPath(heights) {
+function minimumEffortPathA(heights) {
     const m = heights.length, n = heights[0].length;
     const vi = Array.from({ length: m }, () => Array.from({ length: n }, () => 0));
 
@@ -33,8 +35,7 @@ function minimumEffortPath(heights) {
         return false;
     }
 
-
-    let l = 0, r = 1e6, t = 0;;
+    let l = 0, r = 1e6, t = 0;
     while (l < r) {
         const mid = l + Math.floor((r - l) / 2);
         if (dfs(++t, 0, 0, mid)) {
@@ -46,4 +47,36 @@ function minimumEffortPath(heights) {
     return r;
 }
 
-module.exports = minimumEffortPath;
+/**
+ * binary search
+ * breadth first search
+ * @param {number[][]} heights
+ * @returns {number}
+ */
+function minimumEffortPathB(heights) {
+    const m = heights.length, n = heights[0].length, v = m * n - 1;
+    let l = 0, r = 1e6;
+    while (l < r) {
+        const mid = l + Math.floor((r - l) / 2);
+        const que = [[0, 0]];
+        const vis = new Set([0]);
+        while (que.length) {
+            const [i, j] = que.shift();
+            for (const [a, b] of [[0, 1], [0, -1], [1, 0], [-1, 0]]) {
+                const x = i + a, y = j + b;
+                if (x >= 0 && x < m && y >= 0 && y < n && !vis.has(x * n + y) && Math.abs(heights[i][j] - heights[x][y]) <= mid) {
+                    que.push([x, y]);
+                    vis.add(x * n + y);
+                }
+            }
+        }
+        if (vis.has(v)) {
+            r = mid;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return r;
+}
+
+module.exports = { minimumEffortPathA, minimumEffortPathB };
--- test/leetcode/n1631/solution.test.js
+++ test/leetcode/n1631/solution.test.js
@@ -1,22 +1,32 @@
-const minimumEffortPath = require('src/leetcode/n1631/solution.js')
+const { minimumEffortPathA, minimumEffortPathB } = require('src/leetcode/n1631/solution.js')
 
 
 test('test_1', () => {
     const heights = [[1, 2, 2], [3, 8, 2], [5, 3, 5]];
-    expect(minimumEffortPath(heights)).toBe(2);
+    expect(minimumEffortPathA(heights)).toBe(2);
+    expect(minimumEffortPathB(heights)).toBe(2);
 });
 
 test('test_2', () => {
     const heights = [[1, 2, 3], [3, 8, 4], [5, 3, 5]];
-    expect(minimumEffortPath(heights)).toBe(1);
+    expect(minimumEffortPathA(heights)).toBe(1);
+    expect(minimumEffortPathB(heights)).toBe(1);
 });
 
 test('test_3', () => {
     const heights = [[1, 2, 1, 1, 1], [1, 2, 1, 2, 1], [1, 2, 1, 2, 1], [1, 2, 1, 2, 1], [1, 1, 1, 2, 1]];
-    expect(minimumEffortPath(heights)).toBe(0);
+    expect(minimumEffortPathA(heights)).toBe(0);
+    expect(minimumEffortPathB(heights)).toBe(0);
 });
 
 test('test_4', () => {
     const heights = [[8, 3, 2, 5, 2, 10, 7, 1, 8, 9], [1, 4, 9, 1, 10, 2, 4, 10, 3, 5], [4, 10, 10, 3, 6, 1, 3, 9, 8, 8], [4, 4, 6, 10, 10, 10, 2, 10, 8, 8], [9, 10, 2, 4, 1, 2, 2, 6, 5, 7], [2, 9, 2, 6, 1, 4, 7, 6, 10, 9], [8, 8, 2, 10, 8, 2, 3, 9, 5, 3], [2, 10, 9, 3, 5, 1, 7, 4, 5, 6], [2, 3, 9, 2, 5, 10, 2, 7, 1, 8], [9, 10, 4, 10, 7, 4, 9, 3, 1, 6]];
-    expect(minimumEffortPath(heights)).toBe(5);
+    expect(minimumEffortPathA(heights)).toBe(5);
+    expect(minimumEffortPathB(heights)).toBe(5);
+});
+
+test('test_5', () => {
+    const heights = [[2, 3, 6, 3, 6, 6, 1, 2], [4, 5, 6, 5, 5, 10, 1, 2], [9, 1, 4, 10, 4, 7, 7, 3]];
+    expect(minimumEffortPathA(heights)).toBe(4);
+    expect(minimumEffortPathB(heights)).toBe(4);
 });
