deecf00df405ae8dbd397a8c3200979d9cb0ec64
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-05-16 03:01:09 +0000

    1631 path with minimum effort dijkstra

--- src/leetcode/n1631/solution.js
+++ src/leetcode/n1631/solution.js
@@ -79,4 +79,107 @@ function minimumEffortPathB(heights) {
     return r;
 }
 
-module.exports = { minimumEffortPathA, minimumEffortPathB };
+/**
+ * dijkstra
+ * @param {number[][]} heights
+ */
+function minimumEffortPathC(heights) {
+
+    class Heap {
+
+        /**
+         * @type {number[][]}
+         */
+        #heap = [null];
+
+        /**
+         * @param {number[]} val
+         */
+        push(val) {
+            this.#heap.push(val);
+            this.#float(this.#heap.length - 1);
+        }
+
+        /**
+         * @param {number} i
+         */
+        #float(i) {
+            while (i > 1) {
+                const j = Math.floor(i / 2);
+                if (this.#heap[i][2] >= this.#heap[j][2]) {
+                    break;
+                }
+                [this.#heap[i], this.#heap[j]] = [this.#heap[j], this.#heap[i]];
+                i = j;
+            }
+        }
+
+        /**
+         * @returns {number[]}
+         */
+        pop() {
+            if (this.#heap.length == 1) {
+                throw new Error("heap is empty");
+            }
+            if (this.#heap.length == 2) {
+                return this.#heap.pop();
+            }
+            const val = this.#heap[1];
+            this.#heap[1] = this.#heap.pop();
+            this.#sink(1);
+            return val;
+        }
+
+        /**
+         * @param {number} i
+         */
+        #sink(i) {
+            while (i * 2 < this.#heap.length) {
+                const [l, r] = [i * 2, i * 2 + 1];
+                let j = i;
+                if (this.#heap[j][2] > this.#heap[l][2]) {
+                    j = l;
+                }
+                if (r < this.#heap.length && this.#heap[j][2] > this.#heap[r][2]) {
+                    j = r;
+                }
+                if (i == j) {
+                    break;
+                }
+                [this.#heap[i], this.#heap[j]] = [this.#heap[j], this.#heap[i]];
+                i = j;
+            }
+        }
+
+        /**
+         * @returns {boolean}
+         */
+        isEmpty() {
+            return this.#heap.length == 1;
+        }
+
+    }
+
+    const [m, n] = [heights.length, heights[0].length];
+    const visited = Array.from({ length: m }, () => Array.from({ length: n }, () => false));
+    const efforts = Array.from({ length: m }, () => Array.from({ length: n }, () => Infinity));
+    efforts[0][0] = 0;
+    const heap = new Heap();
+    heap.push([0, 0, 0]);
+    while (!heap.isEmpty()) {
+        const [i, j, _] = heap.pop();
+        if (!visited[i][j]) {
+            visited[i][j] = true;
+            for (const [a, b] of [[0, 1], [0, -1], [1, 0], [-1, 0]]) {
+                const [x, y] = [i + a, j + b];
+                if (x >= 0 && x < m && y >= 0 && y < n) {
+                    efforts[x][y] = Math.min(efforts[x][y], Math.max(efforts[i][j], Math.abs(heights[i][j] - heights[x][y])));
+                    heap.push([x, y, efforts[x][y]]);
+                }
+            }
+        }
+    }
+    return efforts[m - 1][n - 1];
+}
+
+module.exports = { minimumEffortPathA, minimumEffortPathB, minimumEffortPathC };
--- test/leetcode/n1631/solution.test.js
+++ test/leetcode/n1631/solution.test.js
@@ -1,32 +1,44 @@
-const { minimumEffortPathA, minimumEffortPathB } = require('src/leetcode/n1631/solution.js')
+const { minimumEffortPathA, minimumEffortPathB, minimumEffortPathC } = require('src/leetcode/n1631/solution.js')
 
 
 test('test_1', () => {
     const heights = [[1, 2, 2], [3, 8, 2], [5, 3, 5]];
     expect(minimumEffortPathA(heights)).toBe(2);
     expect(minimumEffortPathB(heights)).toBe(2);
+    expect(minimumEffortPathC(heights)).toBe(2);
 });
 
 test('test_2', () => {
     const heights = [[1, 2, 3], [3, 8, 4], [5, 3, 5]];
     expect(minimumEffortPathA(heights)).toBe(1);
     expect(minimumEffortPathB(heights)).toBe(1);
+    expect(minimumEffortPathC(heights)).toBe(1);
 });
 
 test('test_3', () => {
     const heights = [[1, 2, 1, 1, 1], [1, 2, 1, 2, 1], [1, 2, 1, 2, 1], [1, 2, 1, 2, 1], [1, 1, 1, 2, 1]];
     expect(minimumEffortPathA(heights)).toBe(0);
     expect(minimumEffortPathB(heights)).toBe(0);
+    expect(minimumEffortPathC(heights)).toBe(0);
 });
 
 test('test_4', () => {
     const heights = [[8, 3, 2, 5, 2, 10, 7, 1, 8, 9], [1, 4, 9, 1, 10, 2, 4, 10, 3, 5], [4, 10, 10, 3, 6, 1, 3, 9, 8, 8], [4, 4, 6, 10, 10, 10, 2, 10, 8, 8], [9, 10, 2, 4, 1, 2, 2, 6, 5, 7], [2, 9, 2, 6, 1, 4, 7, 6, 10, 9], [8, 8, 2, 10, 8, 2, 3, 9, 5, 3], [2, 10, 9, 3, 5, 1, 7, 4, 5, 6], [2, 3, 9, 2, 5, 10, 2, 7, 1, 8], [9, 10, 4, 10, 7, 4, 9, 3, 1, 6]];
     expect(minimumEffortPathA(heights)).toBe(5);
     expect(minimumEffortPathB(heights)).toBe(5);
+    expect(minimumEffortPathC(heights)).toBe(5);
 });
 
 test('test_5', () => {
     const heights = [[2, 3, 6, 3, 6, 6, 1, 2], [4, 5, 6, 5, 5, 10, 1, 2], [9, 1, 4, 10, 4, 7, 7, 3]];
     expect(minimumEffortPathA(heights)).toBe(4);
     expect(minimumEffortPathB(heights)).toBe(4);
+    expect(minimumEffortPathC(heights)).toBe(4);
+});
+
+test('test_6', () => {
+    const heights = [[1, 5, 5, 9, 10, 10, 1, 7], [3, 5, 6, 6, 9, 9, 4, 8], [10, 5, 10, 9, 2, 2, 8, 6], [3, 9, 5, 3, 6, 8, 6, 5]];
+    expect(minimumEffortPathA(heights)).toBe(4);
+    expect(minimumEffortPathB(heights)).toBe(4);
+    expect(minimumEffortPathC(heights)).toBe(4);
 });
