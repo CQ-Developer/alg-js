dc3001163333d75e8d5e9623ec2696947ea2c562
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-05-23 03:49:01 +0000

    3281 maximize score of numbers in ranges binary search + greedy

--- src/leetcode/n3281/solution.js
+++ src/leetcode/n3281/solution.js
@@ -1,9 +1,43 @@
 /**
  * @param {number[]} start
  * @param {number} d
+ * @param {number}
  */
 function maxPossibleScore(start, d) {
     throw new Error('skip');
 }
 
-module.exports = { maxPossibleScore };
+/**
+ * @param {number[]} start
+ * @param {number} d
+ * @returns {number}
+ */
+function maxPossibleScoreA(start, d) {
+    /**
+     * @param {number} m
+     * @returns {boolean}
+     */
+    function check(m) {
+        let p = -m;
+        for (const s of start) {
+            if (p + m > s + d) {
+                return false;
+            }
+            p = p + m <= s ? s : p + m;
+        }
+        return true;
+    }
+    start.sort((a, b) => a - b);
+    let [l, r] = [0, start.at(-1) + d];
+    while (l <= r) {
+        const m = l + Math.floor((r - l) / 2);
+        if (check(m)) {
+            l = m + 1;
+        } else {
+            r = m - 1;
+        }
+    }
+    return r;
+}
+
+module.exports = { maxPossibleScore, maxPossibleScoreA };
--- test/leetcode/n3281/solution.test.js
+++ test/leetcode/n3281/solution.test.js
@@ -1,13 +1,16 @@
-const { MaxPriorityQueue } = require('@datastructures-js/priority-queue');
-const { maxPossibleScore } = require('src/leetcode/n3281/solution');
+const { maxPossibleScore, maxPossibleScoreA } = require('src/leetcode/n3281/solution');
 
 const data = [
     { start: [6, 0, 3], d: 2, result: 4 },
-    { start: [2, 6, 13, 13], d: 2, result: 5 }
+    { start: [2, 6, 13, 13], d: 5, result: 5 }
 ];
 
 const f = test.each(data);
 
 f('maxPossibleScore($start, $d)', ({ start, d }) => {
-    expect(() => MaxPriorityQueue(start, d)).toThrow();
+    expect(() => maxPossibleScore(start, d)).toThrow();
+});
+
+f('maxPossibleScoreA($start, $d)', ({ start, d, result }) => {
+    expect(maxPossibleScoreA(start, d)).toBe(result);
 });
