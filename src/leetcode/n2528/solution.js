6f29d31e4acc294b490359eb973ecb21f48acced
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-05-27 02:25:15 +0000

    2528 maximize the minimum powered city

---
+++ src/leetcode/n2528/solution.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} stations
+ * @param {number} r
+ * @param {number} k
+ * @returns {number}
+ */
+function maxPower(stations, r, k) {
+    throw new Error('skip');
+}
+
+module.exports = { maxPower };
---
+++ test/leetcode/n2528/solution.test.js
@@ -0,0 +1,12 @@
+const { maxPower } = require('src/leetcode/n2528/solution');
+
+const data = [
+    { stations: [1, 2, 4, 5, 0], r: 1, k: 2, result: 5 },
+    { stations: [4, 4, 4, 4], r: 0, k: 3, result: 4 }
+];
+
+const f = test.each(data);
+
+f('maxPower($stations, $r, $k)', ({ stations, r, k }) => {
+    expect(() => maxPower(stations, r, k)).toThrow('skip');
+});
