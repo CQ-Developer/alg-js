a393207f0e21125f6b9c36a06d9a0f270797a272
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-06-04 09:32:38 +0000

    3464 maximize the distance between points on a square

---
+++ src/leetcode/n3464/solution.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} side
+ * @param {number[][]} points
+ * @param {number} k
+ * @returns {number}
+ */
+function maxDistance(side, points, k) {
+    return 0;
+}
+
+module.exports = { maxDistance };
---
+++ test/leetcode/n3464/solution.test.js
@@ -0,0 +1,13 @@
+const { maxDistance } = require('src/leetcode/n3464/solution');
+
+const data = [
+    { side: 2, points: [[0, 2], [2, 0], [2, 2], [0, 0]], k: 4, ans: 2 },
+    { side: 1, points: [[0, 0], [1, 2], [2, 0], [2, 2], [2, 1]], k: 4, ans: 1 },
+    { side: 2, points: [[0, 0], [0, 1], [0, 2], [1, 2], [2, 0], [2, 2], [2, 1]], k: 5, ans: 1 }
+];
+
+const f = test.each(data);
+
+f('maxDistance($side, $points, $k)', ({ side, points, k, ans }) => {
+    expect(maxDistance(side, points, k)).toBe(ans);
+});
