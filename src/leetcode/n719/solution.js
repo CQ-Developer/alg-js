32d330e72e2656cdcb517949a26584d55692c3ee
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-06-12 07:13:25 +0000

    719. find k-th smallest pair distance binary search + sliding window

--- src/leetcode/n719/solution.js
+++ src/leetcode/n719/solution.js
@@ -1,9 +1,20 @@
 /**
+ * @abstract
  * @param {number[]} nums
  * @param {number} k
  * @returns {number}
  */
 function smallestDistancePair(nums, k) {
+    throw new Error('skip');
+
+}
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @returns {number}
+ */
+function smallestDistancePairA(nums, k) {
     const n = nums.length;
     /**
      * @param {number} x
@@ -32,4 +43,40 @@ function smallestDistancePair(nums, k) {
     return l;
 }
 
-module.exports = { smallestDistancePair };
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @returns {number}
+ */
+function smallestDistancePairB(nums, k) {
+    nums.sort((a, b) => a - b);
+    const n = nums.length;
+
+    /**
+     * @param {number} x
+     * @returns {number}
+     */
+    function check(x) {
+        let cnt = 0;
+        for (let [l, r] = [0, 0]; r < n; r++) {
+            while (nums[r] - nums[l] > x) {
+                l++;
+            }
+            cnt += r - l;
+        }
+        return cnt;
+    }
+
+    let [l, r] = [0, nums[n - 1] - nums[0]];
+    while (l <= r) {
+        const mid = l + Math.floor((r - l) / 2);
+        if (check(mid) >= k) {
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return l;
+}
+
+module.exports = { smallestDistancePair, smallestDistancePairA, smallestDistancePairB };
--- test/leetcode/n719/solution.test.js
+++ test/leetcode/n719/solution.test.js
@@ -1,4 +1,4 @@
-const { smallestDistancePair } = require('src/leetcode/n719/solution');
+const { smallestDistancePair, smallestDistancePairA, smallestDistancePairB } = require('src/leetcode/n719/solution');
 
 const cases = [
     { nums: [1, 3, 1], k: 1, ans: 0 },
@@ -8,6 +8,14 @@ const cases = [
 
 const f = test.each(cases);
 
-f('smallestDistancePair($nums, $k)', ({ nums, k, ans }) => {
-    expect(smallestDistancePair(nums, k)).toBe(ans);
+f('smallestDistancePair($nums, $k)', ({ nums, k }) => {
+    expect(() => smallestDistancePair(nums, k)).toThrow('skip');
+});
+
+f('smallestDistancePairA($nums, $k)', ({ nums, k, ans }) => {
+    expect(smallestDistancePairA(nums, k)).toBe(ans);
+});
+
+f('smallestDistancePairB($nums, $k)', ({ nums, k, ans }) => {
+    expect(smallestDistancePairB(nums, k)).toBe(ans);
 });
