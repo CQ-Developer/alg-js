39af20122e422ef02bd169f02d44f5f53021c679
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-06-18 06:21:07 +0000

    373. find k pairs with smallest sums binary search

--- src/leetcode/n373/solution.js
+++ src/leetcode/n373/solution.js
@@ -56,4 +56,68 @@ function kSmallestPairsB(nums1, nums2, k) {
     return res;
 }
 
-module.exports = { kSmallestPairs, kSmallestPairsA, kSmallestPairsB };
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k
+ * @returns {number[][]}
+ */
+function kSmallestPairsC(nums1, nums2, k) {
+    const [m, n] = [nums1.length, nums2.length];
+    /**
+     * @param {number} x
+     * @returns {number}
+     */
+    function check(x) {
+        let [cnt, l, r] = [0, 0, n - 1];
+        while (l < m && r >= 0) {
+            if (nums1[l] + nums2[r] > x) {
+                r--;
+            } else {
+                l++;
+                cnt += r + 1;
+            }
+        }
+        return cnt;
+    }
+    let [left, right] = [nums1[0] + nums2[0], nums1[m - 1] + nums2[n - 1]];
+    while (left <= right) {
+        const mid = left + Math.floor((right - left) / 2);
+        if (check(mid) >= k) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    const res = [];
+    for (let [l, r] = [0, n - 1]; l < m; l++) {
+        while (r >= 0 && nums1[l] + nums2[r] >= left) {
+            r--;
+        }
+        for (let i = 0; i <= r && k > 0; i++, k--) {
+            res.push([nums1[l], nums2[i]]);
+        }
+    }
+    for (let [l, r] = [0, n - 1]; l < m && k > 0; l++) {
+        const i = l;
+        while (l + 1 < m && nums1[l] == nums1[l + 1]) {
+            l++;
+        }
+        while (r >= 0 && nums1[l] + nums2[r] > left) {
+            r--;
+        }
+        const j = r;
+        while (r > 0 && nums2[r] == nums2[r - 1]) {
+            r--;
+        }
+        if (nums1[l] + nums2[r] == left) {
+            const cnt = Math.min(k, (l - i + 1) * (j - r + 1));
+            for (let c = 0; c < cnt && k > 0; c++, k--) {
+                res.push([nums1[l], nums2[r]]);
+            }
+        }
+    }
+    return res;
+}
+
+module.exports = { kSmallestPairs, kSmallestPairsA, kSmallestPairsB, kSmallestPairsC };
--- test/leetcode/n373/solution.test.js
+++ test/leetcode/n373/solution.test.js
@@ -1,4 +1,4 @@
-const { kSmallestPairs, kSmallestPairsA, kSmallestPairsB } = require('src/leetcode/n373/solution');
+const { kSmallestPairs, kSmallestPairsA, kSmallestPairsB, kSmallestPairsC } = require('src/leetcode/n373/solution');
 
 const data = [
     { nums1: [1, 7, 11], nums2: [2, 4, 6], k: 3, res: [[1, 2], [1, 4], [1, 6]] },
@@ -17,4 +17,8 @@ f('kSmallestPairsA($nums1, $nums2, $k)', ({ nums1, nums2, k, res }) => {
 
 f('kSmallestPairsB($nums1, $nums2, $k)', ({ nums1, nums2, k, res }) => {
     expect(kSmallestPairsB(nums1, nums2, k)).toEqual(res);
+});
+
+f('kSmallestPairsC($nums1, $nums2, $k)', ({ nums1, nums2, k, res }) => {
+    expect(kSmallestPairsC(nums1, nums2, k)).toEqual(res);
 });
