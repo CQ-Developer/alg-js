52c342e7538d85b2063669c55ecba98a9a90fa87
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-06-18 08:51:10 +0000

    1439. find the kth smallest sum of a matrix with sorted rows

---
+++ src/leetcode/n1439/solution.js
@@ -0,0 +1,11 @@
+/**
+ * @abstract
+ * @param {number[][]} mat
+ * @param {number} k
+ * @returns {number}
+ */
+function kthSmallest(mat, k) {
+    throw new Error('skip');
+}
+
+module.exports = { kthSmallest };
---
+++ test/leetcode/n1439/solution.test.js
@@ -0,0 +1,43 @@
+const { kthSmallest } = require('src/leetcode/n1439/solution');
+
+const data = [
+    {
+        mat: [
+            [1, 3, 11],
+            [2, 4, 6]
+        ],
+        k: 5,
+        res: 7
+    },
+    {
+        mat: [
+            [1, 3, 11],
+            [2, 4, 6]
+        ],
+        k: 9,
+        res: 17
+    },
+    {
+        mat: [
+            [1, 10, 10],
+            [1, 4, 5],
+            [2, 3, 6]
+        ],
+        k: 7,
+        res: 9
+    },
+    {
+        mat: [
+            [1, 1, 10],
+            [2, 2, 9]
+        ],
+        k: 7,
+        res: 12
+    }
+];
+
+const fun = test.each(data);
+
+fun('kthSmallest($mat, $k)', ({ mat, k }) => {
+    expect(() => kthSmallest(mat, k)).toThrow('skip');
+});
