89f521224893dd18c6958cfea7744f9822bd07a6
Jack <8551681083@163.com>
2025-06-21 11:30:17 +0000

    786. k-th smallest prime fraction: multiway merge

--- src/leetcode/n786/solution.js
+++ src/leetcode/n786/solution.js
@@ -1,3 +1,5 @@
+const { PriorityQueue } = require('@datastructures-js/priority-queue');
+
 /**
  * @abstract
  * @param {number[]} arr
@@ -8,4 +10,24 @@ function kthSmallestPrimeFraction(arr, k) {
     throw new Error('skip');
 }
 
-module.exports = { kthSmallestPrimeFraction };
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @returns {number[]}
+ */
+function kthSmallestPrimeFractionA(arr, k) {
+    const heap = new PriorityQueue((a, b) => arr[a[0]] * arr[b[1]] - arr[b[0]] * arr[a[1]]);
+    for (let j = 1; j < arr.length; j++) {
+        heap.enqueue([0, j]);
+    }
+    for (let _ = 1; _ < k; _++) {
+        const [i, j] = heap.dequeue();
+        if (i + 1 < j) {
+            heap.enqueue([i + 1, j]);
+        }
+    }
+    const [i, j] = heap.front();
+    return [arr[i], arr[j]];
+}
+
+module.exports = { kthSmallestPrimeFraction, kthSmallestPrimeFractionA };
--- test/leetcode/n786/solution.test.js
+++ test/leetcode/n786/solution.test.js
@@ -1,4 +1,4 @@
-const { kthSmallestPrimeFraction } = require('src/leetcode/n786/solution');
+const { kthSmallestPrimeFraction, kthSmallestPrimeFractionA } = require('src/leetcode/n786/solution');
 
 const data = [
     { arr: [1, 2, 3, 5], k: 3, ans: [2, 5] },
@@ -9,4 +9,8 @@ const f = test.each(data);
 
 f('kthSmallestPrimeFraction($arr, $k)', ({ arr, k }) => {
     expect(() => kthSmallestPrimeFraction(arr, k)).toThrow('skip');
+});
+
+f('kthSmallestPrimeFractionA($arr, $k)', ({ arr, k, ans }) => {
+    expect(kthSmallestPrimeFractionA(arr, k)).toEqual(ans);
 });
