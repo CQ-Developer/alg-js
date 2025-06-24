5b057d03982f185c7fa186eb97ce7bb2f70fe75d
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-06-24 02:38:37 +0000

    3134. find the median of the uniqueness array: binary search + sliding window

--- src/leetcode/n3134/solution.js
+++ src/leetcode/n3134/solution.js
@@ -5,7 +5,50 @@
  */
 function medianOfUniquenessArray(nums) {
     throw new Error('skip');
-
 }
 
-module.exports = { medianOfUniquenessArray };
+/**
+ * @param {number[]} nums
+ * @returns {number}
+ */
+function medianOfUniquenessArrayA(nums) {
+    const n = nums.length;
+    const k = Math.floor((n * (n + 1) / 2 + 1) / 2);
+
+    /**
+     * @param {number} up
+     * @returns {boolean}
+     */
+    function check(up) {
+        let cnt = 0;
+        const freq = new Map();
+        for (let [l, r] = [0, 0]; r < n; r++) {
+            freq.set(nums[r], (freq.get(nums[r]) ?? 0) + 1);
+            for (; freq.size > up; l++) {
+                let num = freq.get(nums[l]) - 1;
+                if (num == 0) {
+                    freq.delete(nums[l]);
+                } else {
+                    freq.set(nums[l], num);
+                }
+            }
+            if ((cnt += r - l + 1) >= k) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    let [l, r] = [0, n];
+    while (l + 1 < r) {
+        const mid = l + (Math.floor((r - l) / 2));
+        if (check(mid)) {
+            r = mid;
+        } else {
+            l = mid;
+        }
+    }
+    return r;
+}
+
+module.exports = { medianOfUniquenessArray, medianOfUniquenessArrayA };
--- test/leetcode/n3134/solution.test.js
+++ test/leetcode/n3134/solution.test.js
@@ -1,7 +1,7 @@
-const { medianOfUniquenessArray } = require('src/leetcode/n3134/solution');
+const { medianOfUniquenessArray, medianOfUniquenessArrayA } = require('src/leetcode/n3134/solution');
 
 const data = [
-    { nms: [1, 2, 3], ans: 1 },
+    { nums: [1, 2, 3], ans: 1 },
     { nums: [3, 4, 3, 4, 5], ans: 2 },
     { nums: [4, 3, 5, 4], ans: 2 }
 ];
@@ -10,4 +10,8 @@ const f = test.each(data);
 
 f('medianOfUniquenessArray($nums)', ({ nums }) => {
     expect(() => medianOfUniquenessArray(nums)).toThrow('skip');
+});
+
+f('medianOfUniquenessArrayA($nums)', ({ nums, ans }) => {
+    expect(medianOfUniquenessArrayA(nums)).toBe(ans);
 });
