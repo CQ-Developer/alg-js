7d37be2a1de3be3b2910d346bc0bb031b384e623
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-06-25 01:52:54 +0000

    2040. kth smallest product of two sorted arrays

---
+++ src/leetcode/n2040/solution.js
@@ -0,0 +1,12 @@
+module.exports = {kthSmallestProduct};
+
+/**
+ * @abstract
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k
+ * @returns {number}
+ */
+function kthSmallestProduct(nums1, nums2, k) {
+    throw new Error('skip');
+}
---
+++ test/leetcode/n2040/solution.test.js
@@ -0,0 +1,13 @@
+const { kthSmallestProduct } = require('src/leetcode/n2040/solution');
+
+const data = [
+    { nums1: [1, 2], nums2: [3, 4], k: 2, ans: 8 },
+    { nums1: [-4, -2, 0, 3], nums2: [2, 4], k: 6, ans: 0 },
+    { nums1: [-2, -1, 0, 1, 2], nums2: [-3, -1, 2, 4, 5], k: 3, ans: -6 }
+];
+
+const f = test.each(data);
+
+f('kthSmallestProduct($nums1, $nums2, $k)', ({ nums1, nums2, k }) => {
+    expect(() => kthSmallestProduct(nums1, nums2, k)).toThrow('skip');
+});
