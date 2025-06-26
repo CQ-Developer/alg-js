7cc7292e2baff67dc687ffb74f734f7d60a32034
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-06-26 00:55:01 +0000

    2386. find the k-sum of an array

---
+++ src/leetcode/n2386/solution.js
@@ -0,0 +1,12 @@
+module.exports = { kSum };
+
+/**
+ * @abstract
+ * @param {number[]} nums
+ * @param {number} k
+ * @returns {number}
+ */
+function kSum(nums, k) {
+    throw new Error('skip');
+
+}
