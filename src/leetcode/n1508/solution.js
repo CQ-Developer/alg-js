532c87d42477947051538336db073e8e5f88a821
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-06-26 08:28:08 +0000

    1508. range sum of sorted subarray sums

---
+++ src/leetcode/n1508/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @abstract
+ * @param {number[]} nums
+ * @param {number} n
+ * @param {number} left
+ * @param {number} right
+ * @returns {number}
+ */
+function rangeSum(nums, n, left, right) {
+    throw new Error('skip');
+}
+
+module.exports = { rangeSum };
---
+++ test/leetcode/n1508/solution.test.js
@@ -0,0 +1,13 @@
+const { rangeSum } = require('src/leetcode/n1508/solution');
+
+const data = [
+    { nums: [1, 2, 3, 4], n: 4, left: 1, right: 5, ans: 13 },
+    { nums: [1, 2, 3, 4], n: 4, left: 3, right: 4, ans: 6 },
+    { nums: [1, 2, 3, 4], n: 4, left: 1, right: 10, ans: 50 }
+];
+
+const f = test.each(data);
+
+f('rangeSum($nums, $n, $left, $right)', ({ nums, n, left, right }) => {
+    expect(() => rangeSum(nums, n, left, right)).toThrow('skip');
+});
