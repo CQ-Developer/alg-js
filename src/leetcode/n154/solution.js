4c992b8f89fc96cd4f401d573115c7c9632e3b2e
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-07-04 00:55:29 +0000

    154. find minimum in rotated sorted array II binary search

--- src/leetcode/n154/solution.js
+++ src/leetcode/n154/solution.js
@@ -3,7 +3,18 @@
  * @returns {number}
  */
 function findMin(nums) {
-    return 0;
+    let [l, r] = [-1, nums.length - 1];
+    while (l + 1 < r) {
+        const i = l + Math.floor((r - l) / 2);
+        if (nums[i] < nums[r]) {
+            r = i;
+        } else if (nums[i] > nums[r]) {
+            l = i;
+        } else {
+            r--;
+        }
+    }
+    return nums[r];
 }
 
 module.exports = { findMin };
