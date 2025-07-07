cb94d7625fb0582358168223ffde4d45846bef31
Jack <8551681083@163.com>
2025-07-07 14:24:13 +0000

    81. search in rotated sorted array II binary search

--- src/leetcode/n81/solution.js
+++ src/leetcode/n81/solution.js
@@ -2,10 +2,41 @@
  * @abstract
  * @param {number[]} nums
  * @param {number} target 
- * @returns {number}
+ * @returns {boolean}
  */
 function search(nums, target) {
     throw new Error('skip');
 }
 
-module.exports = { search };
+/**
+ * @param {number[]} nums
+ * @param {number} target 
+ * @returns {boolean}
+ */
+function searchA(nums, target) {
+    let [l, r] = [0, nums.length - 1];
+    while (l <= r) {
+        const i = l + Math.floor((r - l) / 2);
+        if (nums[i] == target) {
+            return true;
+        }
+        if (nums[l] < nums[i]) {
+            if (nums[i] > target && nums[l] <= target) {
+                r = i - 1;
+            } else {
+                l = i + 1;
+            }
+        } else if (nums[l] > nums[i]) {
+            if (nums[i] < target && nums[r] >= target) {
+                l = i + 1;
+            } else {
+                r = i - 1;
+            }
+        } else {
+            l++;
+        }
+    }
+    return false;
+}
+
+module.exports = { search, searchA };
--- test/leetcode/n81/solution.test.js
+++ test/leetcode/n81/solution.test.js
@@ -1,8 +1,9 @@
-const { search } = require('src/leetcode/n81/solution');
+const { search, searchA } = require('src/leetcode/n81/solution');
 
 test.each([
     { nums: [2, 5, 6, 0, 0, 1, 2], target: 0, expected: true },
     { nums: [2, 5, 6, 0, 0, 1, 2], target: 3, expected: false },
-])('search($nums, $target)', ({ nums, target, result }) => {
+])('search($nums, $target)', ({ nums, target, expected }) => {
     expect(() => search(nums, target)).toThrow('skip');
+    expect(searchA(nums, target)).toBe(expected);
 });
