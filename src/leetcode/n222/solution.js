4054310a8134daa6d472c10ff9ce95d4ee0558d2
Jack <8551681083@163.com>
2025-07-12 07:14:02 +0000

    222. count complete tree nodes dfs

--- src/leetcode/n222/solution.js
+++ src/leetcode/n222/solution.js
@@ -17,4 +17,15 @@ function countNodes(root) {
     throw new Error('skip');
 }
 
-module.exports = { TreeNode, countNodes };
+/**
+ * @param {TreeNode} root
+ * @returns {number}
+ */
+function countNodesA(root) {
+    if (root) {
+        return countNodesA(root.left) + countNodesA(root.right) + 1;
+    }
+    return 0;
+}
+
+module.exports = { TreeNode, countNodes, countNodesA };
--- test/leetcode/n222/solution.test.js
+++ test/leetcode/n222/solution.test.js
@@ -1,9 +1,24 @@
-const { TreeNode, countNodes } = require('src/leetcode/n222/solution');
+const { TreeNode, countNodes, countNodesA } = require('src/leetcode/n222/solution');
 
 test.each([
-    { root: new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3, new TreeNode(6), null)), result: 6 },
-    { root: new TreeNode(), result: 0 },
-    { root: new TreeNode(1), result: 1 }
+    {
+        root: new TreeNode(1,
+            new TreeNode(2,
+                new TreeNode(4),
+                new TreeNode(5)),
+            new TreeNode(3,
+                new TreeNode(6))),
+        result: 6
+    },
+    {
+        root: null,
+        result: 0
+    },
+    {
+        root: new TreeNode(1),
+        result: 1
+    }
 ])('countNodes($root)', ({ root, result }) => {
     expect(() => countNodes(root)).toThrow('skip');
+    expect(countNodesA(root)).toBe(result);
 });
