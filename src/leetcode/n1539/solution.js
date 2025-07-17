936d6d6505f3df1ec9c15d9c24647739c54cb0c6
Jack <8551681083@163.com>
2025-07-17 15:03:02 +0000

    1539. kth missing positive number

---
+++ src/leetcode/n1539/solution.js
@@ -0,0 +1,10 @@
+/**
+ * @abstract
+ * @param {number[]} arr
+ * @param {number} k
+ * @returns {number}
+ */
+function findKthPositive(arr, k) {
+}
+
+module.exports = { findKthPositive };
---
+++ test/leetcode/n1539/solution.test.js
@@ -0,0 +1,10 @@
+const { findKthPositive } = require('../../../src/leetcode/n1539/solution');
+
+const f = test.each([
+    { arr: [2, 3, 4, 7, 11], k: 5, result: 9 },
+    { arr: [1, 2, 3, 4], k: 2, result: 6 }
+]);
+
+f('findKthPositive($arr, $k)', ({ arr, k }) => {
+    expect(findKthPositive(arr, k)).toBeUndefined();
+});
