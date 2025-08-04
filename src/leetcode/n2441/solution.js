2956890d2507a9f695da377ec1ad586ee14a6f1e
chen qiang <chen_qiang@hoperun.com>
2025-08-04 17:07:20 +0800

    2441. largest positive integer that exists with its negative hash表

--- src/leetcode/n2441/solution.js
+++ src/leetcode/n2441/solution.js
@@ -6,4 +6,20 @@
 function findMaxK(nums) {
 }
 
-module.exports = {findMaxK};
+/**
+ * @param {number[]} nums
+ * @returns {number}
+ */
+function findMaxKA(nums) {
+    let res = -1;
+    const caching = new Set();
+    for (const num of nums) {
+        if (caching.has(-num)) {
+            res = Math.max(res, Math.abs(num));
+        }
+        caching.add(num);
+    }
+    return res;
+}
+
+module.exports = {findMaxK, findMaxKA};
--- test/leetcode/n2441/solution.test.js
+++ test/leetcode/n2441/solution.test.js
@@ -1,11 +1,15 @@
-const {findMaxK} = require('../../../src/leetcode/n2441/solution');
+const {findMaxK, findMaxKA} = require('../../../src/leetcode/n2441/solution');
 
 const f = test.each([
-    {nums: [-1, 2, -3, 3], result: 1},
+    {nums: [-1, 2, -3, 3], result: 3},
     {nums: [-1, 10, 6, 7, -7, 1], result: 7},
     {nums: [-10, 8, 6, 7, -2, -3], result: -1}
 ]);
 
 f('findMaxK($nums)', ({nums}) => {
     expect(findMaxK(nums)).toBeUndefined();
+});
+
+f('findMaxKA($nums)', ({nums, result}) => {
+    expect(findMaxKA(nums)).toBe(result);
 });
