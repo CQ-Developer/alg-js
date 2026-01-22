8ec5d078ce64149f6c8ea92ffc05ae29a85255cc
Jack <18551681083@163.com>
2026-01-22 10:14:52 +0800

    2552. count increasing quadruplets

---
+++ src/leetcode/n2552/solution.test.ts
@@ -0,0 +1,14 @@
+import { countQuadruplets } from "./solution.ts";
+
+function register(f: (nums: number[]) => number) {
+    describe("count increasing quadruplets", () => {
+        it.each([
+            { nums: [1, 3, 2, 4, 5], ans: 2 },
+            { nums: [1, 2, 3, 4], ans: 0 }
+        ])(`${f.name}($nums)`, ({ nums, ans }) => {
+            expect(f(nums)).toBe(ans);
+        });
+    });
+}
+
+register(countQuadruplets);
---
+++ src/leetcode/n2552/solution.ts
@@ -0,0 +1,24 @@
+export function countQuadruplets(nums: number[]): number {
+    const n = nums.length;
+    const greater = new Array<number[]>(n);
+    greater[n - 1] = Array.from({ length: n + 1 }, () => 0);
+    for (let k = n - 2; k > 1; k--) {
+        greater[k] = [...greater[k + 1]];
+        for (let x = 1; x < nums[k + 1]; x++) {
+            greater[k][x]++;
+        }
+    }
+    let ans = 0;
+    const less = Array.from({ length: n + 1 }, () => 0);
+    for (let j = 1; j < n - 2; j++) {
+        for (let x = nums[j - 1] + 1; x <= n; x++) {
+            less[x]++;
+        }
+        for (let k = j + 1; k < n - 1; k++) {
+            if (nums[k] < nums[j]) {
+                ans += less[nums[k]] * greater[k][nums[j]];
+            }
+        }
+    }
+    return ans;
+}
