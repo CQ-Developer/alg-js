f2faf3e24f6fb7b02af8606d11aa043f91fcc7e7
Jack <18551681083@163.com>
2026-01-26 20:23:26 +0800

    update project

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+source $HOME/.nvm/nvm.sh
 nvm install v22.22.0
 
 rm -f init.sh
